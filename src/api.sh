f3dbb69758506ffe86c7334060448f00164504a3
johnko <279736+johnko@users.noreply.github.com>
2026-03-08 20:00:59 -0400

    add golang

--- src/api.sh
+++ src/api.sh
@@ -4,8 +4,13 @@ set -euo pipefail
 # renovate: datasource=python-version depName=python packageName=python
 PYTHON_VERSION=3.14.3
 
+# renovate: datasource=golang-version depName=golang packageName=golang
+GOLANG_VERSION=1.25.8
+
 WORKDIR=$(dirname "$0")
 pushd "$WORKDIR"
-mise exec python@$PYTHON_VERSION -- pip install -r requirements.txt
-mise exec python@$PYTHON_VERSION -- uvicorn api.main:app --host 127.0.0.1 --port 8000
+# mise exec python@$PYTHON_VERSION -- pip install -r requirements.txt
+# mise exec python@$PYTHON_VERSION -- uvicorn api.main:app --host 127.0.0.1 --port 8000
+mise exec go@$GOLANG_VERSION -- go clean
+mise exec go@$GOLANG_VERSION -- go run
 popd
